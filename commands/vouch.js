9102c247a4e27ca10185fb56240cad0d5608eae6
guygufygugu-afk <guy.gufy.gugu@gmail.com>
2026-06-23 13:03:28 +0300

    vouch.js

--- commands/vouch.js
+++ commands/vouch.js
@@ -1,7 +1,8 @@
 module.exports = {
     name: 'vouch',
-    execute(message, args) {
-        message.reply("✅ Sistemul de Vouch este activ!");
+    async execute(message, args) {
+        // Răspuns unic
+        await message.reply("✅ Sistemul de Vouch este activ!");
     }
 };
 
